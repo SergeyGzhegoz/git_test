3d8fc1c05906f4e79fc068b3f7fbd68a34d0a8e3
SergeyGzhegoz <s.gz@yandex.ru>
2022-12-11 20:48:06 +0300

    Только класс Circle

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
 from math import pi
 
-class Rectangle:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    def area(self):
-        return self._a * self._b
-
-    def get_height(self):
-        return self._a
-    
-    def get_width(self):
-        return self._b
-
-    def set_height(self, a):
-        self._a = a
-
-    def set_width(self, b):
-        self._b = b
-
-
 class Circle:
     def __init__(self, r):
         self._r = r
@@ -29,9 +8,5 @@ class Circle:
         return pi * (self._r ** 2)
 
 
-main_rect = Rectangle(5, 4)
-print(main_rect.area())
-main_rect.set_height(7)
-main_rect.set_width(8)
 main_circle = Circle(2)
 print(main_circle.area())
