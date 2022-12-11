ebda38e7569ad1c66aa87c68828046b107bce4a3
SergeyGzhegoz <s.gz@yandex.ru>
2022-12-11 20:38:15 +0300

    Создали метод area для Circle

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from math import pi
+
 class Rectangle:
     def __init__(self, a, b):
         self.a = a
@@ -22,6 +24,9 @@ class Rectangle:
 class Circle:
     def __init__(self, r):
         self._r = r
+    
+    def area(self):
+        return pi * (self._r ** 2)
 
 
 main_rect = Rectangle(5, 4)
@@ -29,3 +34,4 @@ print(main_rect.area())
 main_rect.set_height(7)
 main_rect.set_width(8)
 main_circle = Circle(2)
+print(main_circle.area())
